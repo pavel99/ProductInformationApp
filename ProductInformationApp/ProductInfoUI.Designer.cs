e7a0b95a52e33c335d152e8a9419da1f2d9c678a
pavel99 <mailforgithub@gmail.com>
2015-05-16 12:29:59 +0600

    First commit for ProductInformationApp

---
+++ ProductInformationApp/ProductInfoUI.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace ProductInformationApp
+{
+    partial class ProductInfoUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.saveGroupBox = new System.Windows.Forms.GroupBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.quantityTextBox = new System.Windows.Forms.TextBox();
+            this.descriptionTextBox = new System.Windows.Forms.TextBox();
+            this.productCodeTextBox = new System.Windows.Forms.TextBox();
+            this.displayGroupBox = new System.Windows.Forms.GroupBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.displayListView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label4 = new System.Windows.Forms.Label();
+            this.totalQuantityTextBox = new System.Windows.Forms.TextBox();
+            this.saveGroupBox.SuspendLayout();
+            this.displayGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Product Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Description";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Quantity";
+            // 
+            // saveGroupBox
+            // 
+            this.saveGroupBox.Controls.Add(this.saveButton);
+            this.saveGroupBox.Controls.Add(this.quantityTextBox);
+            this.saveGroupBox.Controls.Add(this.descriptionTextBox);
+            this.saveGroupBox.Controls.Add(this.productCodeTextBox);
+            this.saveGroupBox.Controls.Add(this.label1);
+            this.saveGroupBox.Controls.Add(this.label3);
+            this.saveGroupBox.Controls.Add(this.label2);
+            this.saveGroupBox.Location = new System.Drawing.Point(179, 12);
+            this.saveGroupBox.Name = "saveGroupBox";
+            this.saveGroupBox.Size = new System.Drawing.Size(439, 222);
+            this.saveGroupBox.TabIndex = 3;
+            this.saveGroupBox.TabStop = false;
+            this.saveGroupBox.Text = "Save";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(219, 184);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 6;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // quantityTextBox
+            // 
+            this.quantityTextBox.Location = new System.Drawing.Point(99, 134);
+            this.quantityTextBox.Name = "quantityTextBox";
+            this.quantityTextBox.Size = new System.Drawing.Size(195, 20);
+            this.quantityTextBox.TabIndex = 5;
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.Location = new System.Drawing.Point(99, 57);
+            this.descriptionTextBox.Multiline = true;
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.Size = new System.Drawing.Size(195, 57);
+            this.descriptionTextBox.TabIndex = 4;
+            // 
+            // productCodeTextBox
+            // 
+            this.productCodeTextBox.Location = new System.Drawing.Point(99, 23);
+            this.productCodeTextBox.Name = "productCodeTextBox";
+            this.productCodeTextBox.Size = new System.Drawing.Size(195, 20);
+            this.productCodeTextBox.TabIndex = 3;
+            // 
+            // displayGroupBox
+            // 
+            this.displayGroupBox.Controls.Add(this.totalQuantityTextBox);
+            this.displayGroupBox.Controls.Add(this.label4);
+            this.displayGroupBox.Controls.Add(this.showButton);
+            this.displayGroupBox.Controls.Add(this.displayListView);
+            this.displayGroupBox.Location = new System.Drawing.Point(179, 265);
+            this.displayGroupBox.Name = "displayGroupBox";
+            this.displayGroupBox.Size = new System.Drawing.Size(439, 256);
+            this.displayGroupBox.TabIndex = 4;
+            this.displayGroupBox.TabStop = false;
+            this.displayGroupBox.Text = "Display";
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(333, 7);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 1;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // displayListView
+            // 
+            this.displayListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.displayListView.GridLines = true;
+            this.displayListView.Location = new System.Drawing.Point(9, 36);
+            this.displayListView.Name = "displayListView";
+            this.displayListView.Size = new System.Drawing.Size(424, 171);
+            this.displayListView.TabIndex = 0;
+            this.displayListView.UseCompatibleStateImageBehavior = false;
+            this.displayListView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Code";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Description";
+            this.columnHeader2.Width = 266;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Quantity";
+            this.columnHeader3.Width = 295;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(99, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Total Quantity";
+            // 
+            // totalQuantityTextBox
+            // 
+            this.totalQuantityTextBox.Location = new System.Drawing.Point(179, 223);
+            this.totalQuantityTextBox.Name = "totalQuantityTextBox";
+            this.totalQuantityTextBox.Size = new System.Drawing.Size(156, 20);
+            this.totalQuantityTextBox.TabIndex = 3;
+            // 
+            // ProductInfoUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 533);
+            this.Controls.Add(this.displayGroupBox);
+            this.Controls.Add(this.saveGroupBox);
+            this.Name = "ProductInfoUI";
+            this.Text = "Product Info";
+            this.saveGroupBox.ResumeLayout(false);
+            this.saveGroupBox.PerformLayout();
+            this.displayGroupBox.ResumeLayout(false);
+            this.displayGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox saveGroupBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox quantityTextBox;
+        private System.Windows.Forms.TextBox descriptionTextBox;
+        private System.Windows.Forms.TextBox productCodeTextBox;
+        private System.Windows.Forms.GroupBox displayGroupBox;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.ListView displayListView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.TextBox totalQuantityTextBox;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ProductInformationApp/ProductInfoUI.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProductInformationApp
+{
+    public partial class ProductInfoUI : Form
+    {
+        public ProductInfoUI()
+        {
+            InitializeComponent();
+        }
+        string connectionString =
+                 ConfigurationManager.ConnectionStrings["ProductDB"].ConnectionString;
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+
+
+            if (productCodeTextBox.Text == "" || descriptionTextBox.Text == "" || quantityTextBox.Text == "")
+            {
+                MessageBox.Show("Please enter all values");
+            }
+            else
+            {
+                string productCode = productCodeTextBox.Text;
+                string description = descriptionTextBox.Text;
+                int quantity = Convert.ToInt32(quantityTextBox.Text);
+
+                if (IsCodeAlreadyExists(productCode))
+                {
+                    if (quantity > -1)
+                    {
+                        SqlConnection connection = new SqlConnection(connectionString);
+
+                        string query = "UPDATE Products SET Quantity=(Quantity +'" + quantity + "')";
+
+                        SqlCommand command = new SqlCommand(query, connection);
+
+                        connection.Open();
+                        int rowAffected = command.ExecuteNonQuery();
+                        connection.Close();
+
+                        if (rowAffected > 0)
+                        {
+                            MessageBox.Show("Saved Successfully!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Saving failed");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Quatity must be positive number");
+                    }
+
+
+                }
+                else
+                {
+
+
+
+
+                    SqlConnection connection = new SqlConnection(connectionString);
+                    if (productCode.Length <= 3 && quantity > -1)
+                    {
+                        string query = "INSERT INTO Products Values('" + productCode + "','" + description + "','" +
+                                       quantity + "')";
+                        SqlCommand command = new SqlCommand(query, connection);
+                        connection.Open();
+                        int rowAffected = command.ExecuteNonQuery();
+                        connection.Close();
+                        if (rowAffected > 0)
+                        {
+                            MessageBox.Show("Product information is Saved");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Saving failed");
+                        }
+
+
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Protuct code must 3 character long and quantity must be positive");
+                    }
+
+                }
+            }
+            productCodeTextBox.Clear();
+            descriptionTextBox.Clear();
+            quantityTextBox.Clear();
+        }
+
+        public bool IsCodeAlreadyExists(string productCode)
+        {
+            SqlConnection connection=new SqlConnection(connectionString);
+            string query="SELECT * FROM Products Where Pcode='"+productCode+"'";
+            bool isProductCodeExists = false;
+             SqlCommand command=new SqlCommand(query,connection);
+             connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            while(reader.Read())
+            {
+                isProductCodeExists = true;
+                 break;
+            }
+            reader.Close();
+            connection.Close();
+            return isProductCodeExists;
+
+
+
+
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            displayListView.Items.Clear();
+           SqlConnection connection=new SqlConnection(connectionString);
+            string query = "SELECT * FROM Products";
+
+            SqlCommand command=new SqlCommand(query,connection);
+             connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                ListViewItem item=new ListViewItem(reader["Pcode"].ToString());
+               
+                item.SubItems.Add(reader["Description"].ToString());
+                item.SubItems.Add(reader["Quantity"].ToString());
+                displayListView.Items.Add(item);
+
+            }
+            reader.Close();
+            connection.Close();
+            totalQuantityTextBox.Text = SumOfQuantity().ToString();
+
+        }
+
+        public int SumOfQuantity()
+        {
+            int sumOfQuantity;
+            SqlConnection connection=new SqlConnection(connectionString);
+            string query = "SELECT SUM(Quantity) FROM Products";
+
+            SqlCommand command=new SqlCommand(query,connection);
+             connection.Open();
+            sumOfQuantity =Convert.ToInt32(command.ExecuteScalar());
+            return sumOfQuantity;
+
+        
+        }
+
+        
+       
+
+       
+    }
+}
